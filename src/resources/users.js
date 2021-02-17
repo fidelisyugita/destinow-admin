3938d3249045c50069639129ae52aed54a2f8fa3
Fidelis Yugita <fb46us@gmail.com>
2021-02-17 23:50:14 +0700

    test admin and update gitignote

---
+++ src/resources/users.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { List, Datagrid, TextField, EmailField, UrlField } from "react-admin";
+
+export const UserList = (props) => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <TextField source="name" />
+      <EmailField source="email" />
+      <TextField source="address.street" />
+      <TextField source="phone" />
+      <UrlField source="website" />
+    </Datagrid>
+  </List>
+);
